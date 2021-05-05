1ffdfee0f055393e46078dcefb9cd1d06db57013
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-05-05 19:46:35 +0200

    Add zoom effect on actual life change

--- Graphics/LifeMeterBattery lives/default.lua
+++ Graphics/LifeMeterBattery lives/default.lua
@@ -1,4 +1,5 @@
 local player = Var "Player"
+local curLives = 0;
 
 local t = Def.ActorFrame{
 	LoadFont("Combo numbers")..{
@@ -10,19 +11,27 @@ local t = Def.ActorFrame{
 				self:effectcolor1(1,1,1,1);
 				self:effectcolor2(1,0,0,1);
 			end;
+			curLives = glifemeter:GetTotalLives()
 			self:settext(glifemeter:GetTotalLives());
 			self:zoom(0.6);
 			self:horizalign(center);
 		end;
 		LifeChangedMessageCommand=function(self,params)
 			if params.Player == player then
-				self:settext(params.LivesLeft);
-				if params.LivesLeft <= 5 then
-					self:diffuseshift();
-					self:effectcolor1(1,1,1,1);
-					self:effectcolor2(1,0,0,1);
-				else
-					self:stopeffect();
+				if curLives ~= params.LivesLeft then
+					curLives = params.LivesLeft;
+					self:stoptweening();
+					self:settext(params.LivesLeft);
+					if params.LivesLeft <= 5 then
+						self:diffuseshift();
+						self:effectcolor1(1,1,1,1);
+						self:effectcolor2(1,0,0,1);
+					else
+						self:stopeffect();
+					end;
+					self:zoom(0.75);
+					self:linear(0.1);
+					self:zoom(0.6);
 				end;
 			end;
 		end;
