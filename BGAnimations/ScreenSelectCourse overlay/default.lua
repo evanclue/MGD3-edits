f12bb5ac2392e054a730c1c900be74dd005ff4ff
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-05-11 11:36:58 +0200

    ScreenSelectCourse Fix

--- BGAnimations/ScreenSelectCourse overlay/default.lua
+++ BGAnimations/ScreenSelectCourse overlay/default.lua
@@ -6,15 +6,6 @@ if IsUsingWideScreen() == true then
 end
 
 t[#t+1] = Def.ActorFrame {
-	LoadActor( "../ball2mod" )..{
-		OnCommand=function(self) self:x(SCREEN_CENTER_X+110):y(SCREEN_CENTER_Y):blend('BlendMode_Add'):diffusealpha(0.5):zoom(6):spin():effectmagnitude(44,67,37) end;
-	};
-	LoadActor( "../ball2mod" )..{
-		OnCommand=function(self) self:Center():z(30):blend('BlendMode_Add'):diffusealpha(0.1):zoom(18):spin():effectmagnitude(0,44,0) end;
-	};
-	LoadActor( "../sphere_back" )..{
-		OnCommand=function(self) self:Center():zoom(7):spin():effectmagnitude(0,124,0):diffusealpha(0.5) end;
-	};
 	LoadActor( "tile" )..{
 		OnCommand=function(self) self:blend('BlendMode_Add'):diffusealpha(0.2) end;
 	};
