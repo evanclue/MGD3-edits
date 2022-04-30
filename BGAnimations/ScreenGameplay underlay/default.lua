a6483e84c06cf80957cfc4adc6da4e85b44ec49b
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-04-30 09:41:29 +0200

    Misbehaves on first play and isn't needed at all

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -3,7 +3,6 @@ local Center1Player = PREFSMAN:GetPreference('Center1Player');
 local NumPlayers = GAMESTATE:GetNumPlayersEnabled();
 local NumSides = GAMESTATE:GetNumSidesJoined();
 local st = GAMESTATE:GetCurrentStyle():GetStepsType();
-local Song = GAMESTATE:GetCurrentSong();
 
 local function GetPosition(pn)
 	if st == "StepsType_Dance_Solo" then
@@ -24,27 +23,6 @@ local function GetPosition(pn)
 		local strPlayer = (NumPlayers == 1) and "OnePlayer" or "TwoPlayers";
 		local strSide = (NumSides == 1) and "OneSide" or "TwoSides";
 		return THEME:GetMetric("ScreenGameplay","Player".. ToEnumShortString(pn) .. strPlayer .. strSide .."X");
-	end;
-end;
-
-if Song then
-	if GAMESTATE:PlayerIsUsingModifier(PLAYER_1,'StaticBG') or GAMESTATE:PlayerIsUsingModifier(PLAYER_2,'StaticBG') or Song:HasBGChanges() then
-	else
-		t[#t+1] = Def.ActorFrame {
-			Def.Sprite{
-				BeginCommand=function(self) self:scale_or_crop_background() end;
-				InitCommand=function(self)
-					if Song then
-						if Song:HasBackground() then
-								self:LoadBackground(Song:GetBackgroundPath());
-							else
-								self:Load(THEME:GetPathG("Common fallback", "background"));
-						end;
-					end;
-				end;
-				OnCommand=function(self) self:diffusealpha(0.5) end;
-			};
-		};
 	end;
 end;
 
