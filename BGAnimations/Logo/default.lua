4e1fc1bfa16ca59cb636919aee4efd1237a27365
Evan Clue <72628412+evanclue@users.noreply.github.com>
2022-09-03 04:03:16 -0400

    fixed the title screen making you wait before letting you press start (#1)

--- BGAnimations/Logo/default.lua
+++ BGAnimations/Logo/default.lua
@@ -4,7 +4,7 @@ return Def.ActorFrame {
 	};
 	LoadFont("Panedisplay Blurred")..{
 		OnCommand=function(self) self:y(200):horizalign(center):maxwidth(573):zoomx(0.3):zoomy(0.2):diffusealpha(0.5):playcommand("Refresh") end;
-		RefreshCommand=function(self) 
+		RefreshCommand=function(self)
 			self:settext(ProductFamily() .. " " .. ProductVersion() .. " (" .. VersionDate() .. ")")
 		end;
 	};
@@ -12,15 +12,18 @@ return Def.ActorFrame {
 		OnCommand=function(self) self:y(222) end;
 	};
 	LoadActor( "effect" )..{
-		OnCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.349):blend('BlendMode_Add'):diffusealpha(0.4):zoom(0.88):decelerate(1.08):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
+        OnCommand=function(self) self:queuecommand("Animate") end,
+		AnimateCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.349):blend('BlendMode_Add'):diffusealpha(0.4):zoom(0.88):decelerate(1.08):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
 		RepeatCommand=function(self) self:sleep(0.4):queuecommand("On") end;
 	};
 	LoadActor( "effect" )..{
-		OnCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.449):blend('BlendMode_Add'):diffusealpha(0.2):zoom(0.88):decelerate(0.98):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
+        OnCommand=function(self) self:queuecommand("Animate") end,
+		AnimateCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.449):blend('BlendMode_Add'):diffusealpha(0.2):zoom(0.88):decelerate(0.98):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
 		RepeatCommand=function(self) self:sleep(0.4):queuecommand("On") end;
 	};
 	LoadActor( "effect" )..{
-		OnCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.549):blend('BlendMode_Add'):diffusealpha(0.1):zoom(0.88):decelerate(0.88):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
+        OnCommand=function(self) self:queuecommand("Animate") end,
+		AnimateCommand=function(self) self:diffusealpha(0):y(-35):sleep(0.549):blend('BlendMode_Add'):diffusealpha(0.1):zoom(0.88):decelerate(0.88):diffusealpha(0):zoom(1):queuecommand("Repeat") end;
 		RepeatCommand=function(self) self:sleep(0.4):queuecommand("On") end;
 	};
 	LoadActor( "mgdlogo" )..{
