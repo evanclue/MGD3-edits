c4fe6f64643aad178eed226fe62a84fa943f2cb6
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-09-09 08:38:51 +0200

    More 2Player stuff

--- BGAnimations/ScreenSelectMusic overlay/accel.lua
+++ BGAnimations/ScreenSelectMusic overlay/accel.lua
@@ -1,45 +1,85 @@
-local SpeedMods = {"0.5x", "1x", "1.5x", "2x", "2.5x", "3x", "3.5x", "4x", "4.5x", "5x", "5.5x", "6x"}
-local sIdx = 2
+local SpeedMods = {"0.5X", "1X", "1.5X", "2X", "2.5X", "3X", "3.5X", "4X", "4.5X", "5X", "5.5X", "6X"}
+local sIdx = {PLAYER_1=2,PLAYER_2=2}
 local sMax = #SpeedMods
 local invert = ThemePrefs.Get("InvertAccel") and -1 or 1
 
-for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
-	if GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):ScrollSpeed() then
-		sIdx = GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):ScrollSpeed() * 2
-	end
-end
-
 return Def.ActorFrame{
+	OnCommand=function()
+		for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
+			if GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):ScrollSpeed() then
+				sIdx[pn] = GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):ScrollSpeed() * 2
+			end
+		end
+	end,
+	PlayerJoinedMessageCommand=function(self)
+		self:playcommand("On")
+	end,
+	PlayerUnjoinedMessageCommand=function(self)
+		self:playcommand("On")
+	end,
 	Def.ActorFrame {
 		CodeMessageCommand = function(self, params)
 			if params.Name == 'SpeedUp' then
-				sIdx = sIdx - (1 * invert)
-				if sIdx < 1 then
-					sIdx = sMax
-				elseif sIdx > sMax then
-					sIdx = 1
+				sIdx[params.PlayerNumber] = sIdx[params.PlayerNumber] - (1 * invert)
+				if sIdx[params.PlayerNumber] < 1 then
+					sIdx[params.PlayerNumber] = sMax
+				elseif sIdx[params.PlayerNumber] > sMax then
+					sIdx[params.PlayerNumber] = 1
 				end
-				GAMESTATE:ApplyGameCommand('mod,'..SpeedMods[sIdx])
-				self:playcommand("Speed")
+				GAMESTATE:ApplyGameCommand('mod,'..SpeedMods[sIdx[params.PlayerNumber]],params.PlayerNumber)
+				self:playcommand("Speed"):playcommand("Speed"..ToEnumShortString(params.PlayerNumber))
 			elseif params.Name == 'SpeedDown' then
-				sIdx = sIdx + (1 * invert)
-				if sIdx > sMax then
-					sIdx = 1
-				elseif sIdx < 1 then
-					sIdx = sMax
+				sIdx[params.PlayerNumber] = sIdx[params.PlayerNumber] + (1 * invert)
+				if sIdx[params.PlayerNumber] > sMax then
+					sIdx[params.PlayerNumber] = 1
+				elseif sIdx[params.PlayerNumber] < 1 then
+					sIdx[params.PlayerNumber] = sMax
 				end
-				GAMESTATE:ApplyGameCommand('mod,'..SpeedMods[sIdx])
-				self:playcommand("Speed")
+				GAMESTATE:ApplyGameCommand('mod,'..SpeedMods[sIdx[params.PlayerNumber]],params.PlayerNumber)
+				self:playcommand("Speed"):playcommand("Speed"..ToEnumShortString(params.PlayerNumber))
 			end
 		end,
 		LoadActor(THEME:GetPathS("ScreenSelectMusic","options"))..{
 			OnCommand=function(self) self:stop() end,
-			SpeedCommand=function(self) self:play() end
+			SupportPan=true,
+			SpeedCommand=function(self)
+				if GAMESTATE:GetNumPlayersEnabled() == 1 then self:play() end
+			end,
+			SpeedP1Command=function(self)
+				if GAMESTATE:GetNumPlayersEnabled() == 2 then self:playforplayer(PLAYER_1) end
+			end,
+			SpeedP2Command=function(self)
+				if GAMESTATE:GetNumPlayersEnabled() == 2 then self:playforplayer(PLAYER_2) end
+			end
 		},
 		LoadFont("OptionIcon")..{
-			OnCommand=function(self) self:x(SCREEN_CENTER_X+100):y(SCREEN_BOTTOM-50):settext(string.upper(SpeedMods[sIdx])):diffuse(color('#BEC1C6')):zoom(0.76) end,
-			SpeedCommand=function(self)
-				self:settext(string.upper(SpeedMods[sIdx]))
+			OnCommand=function(self)
+				if GAMESTATE:GetNumPlayersEnabled() == 1 then
+					self:x(SCREEN_CENTER_X+100)
+				else
+					self:x(SCREEN_CENTER_X+50)
+				end
+				if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+					self:y(SCREEN_BOTTOM-50):settext(string.upper(SpeedMods[sIdx[PLAYER_1]])):diffuse(color('#BEC1C6')):zoom(0.76)
+				end
+			end,
+			SpeedP1Command=function(self,params)
+				self:settext(string.upper(SpeedMods[sIdx[PLAYER_1]]))
+			end
+		},
+		LoadFont("OptionIcon")..{
+			OnCommand=function(self)
+				if GAMESTATE:GetNumPlayersEnabled() == 1 then
+					self:x(SCREEN_CENTER_X+100)
+				else
+					self:x(SCREEN_CENTER_X+150)
+				end
+				if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+					self:y(SCREEN_BOTTOM-50):settext(string.upper(SpeedMods[sIdx[PLAYER_2]])):diffuse(color('#BEC1C6')):zoom(0.76)
+				end
+			end,
+			SpeedP2Command=function(self,params)
+				self:settext(string.upper(SpeedMods[sIdx[PLAYER_2]]))
 			end
 		}
 	}
