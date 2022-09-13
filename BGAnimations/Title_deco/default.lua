c1365340e9db1da1b83f13c9d8c71bce19d9e857
Evan Clue <72628412+evanclue@users.noreply.github.com>
2022-09-13 16:40:30 -0400

    fixed monakai logo alignment

--- BGAnimations/Title_deco/default.lua
+++ BGAnimations/Title_deco/default.lua
@@ -3,7 +3,7 @@ return Def.ActorFrame{
 		OnCommand=function(self) self:Center():addy(126) end
 	},
 	LoadActor("monakai")..{
-		OnCommand=function(self) self:x(SCREEN_RIGHT-72):y(SCREEN_BOTTOM-25):zoom(0.2) end
+		OnCommand=function(self) self:x(SCREEN_RIGHT-61):y(SCREEN_BOTTOM-25):zoom(0.2) end
 	},
 	LoadActor("vulp_logo")..{
 		OnCommand=function(self) self:x(SCREEN_LEFT+69):y(SCREEN_BOTTOM-18):zoom(0.2) end
